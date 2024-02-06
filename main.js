7de48df0fd0b3ed14c78df9d3db4928a1994f344
VitorEmanuel22 <142932850+VitorEmanuel22@users.noreply.github.com>
2024-02-06 16:28:07 -0300

    oi
    oi

---
+++ main.js
@@ -0,0 +1,58 @@
+let canvas = document.getElementById("meuCanvas");
+let ctx = canvas.getContext("2d");
+let color="blue";
+let largura="2";
+
+let mouseX;
+let mouseY;
+
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=largura;
+ctx.arc(200,200,40,0,2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle="black";
+ctx.lineWidth=largura;
+ctx.arc(300,200,40,0,2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle="red";
+ctx.lineWidth=largura;
+ctx.arc(400,200,40,0,2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle="yellow";
+ctx.lineWidth=largura;
+ctx.arc(250,250,40,0,2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle="green";
+ctx.lineWidth=largura;
+ctx.arc(350,250,40,0,2*Math.PI);
+ctx.stroke();
+
+canvas.addEventListener("mousedown", cliquei);
+function cliquei(e){
+      color=document.getElementById("cor").value;
+      largura=document.getElementById("largura").value;
+      mouseX = e.clientX - canvas.offsetLeft;
+      mouseY = e.clientY - canvas.offsetTop;
+      desenharCirculo(mouseX,mouseY);
+}
+
+function desenharCirculo(mouseX,mouseY){
+    ctx.beginPath();
+    ctx.strokeStyle=color;
+    ctx.lineWidth=largura;
+    ctx.arc(mouseX,mouseY,40,0,2*Math.PI);
+    ctx.stroke();
+}
+
+function limpar(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+}
